750d3bfc460f9ebd096037db0c6c05294349de91
diegomebro <diegomatias.mb@hotmail.com>
2016-07-04 20:33:27 -0400

    end form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def index
   end
+  def save_user
+
+  	@nombre=params[:txt_nombre]
+  	@email=params[:txt_mail]
+  	@edad=params[:txt_edad]
+
+  	@newuser= User.new(name: @nombre, email: @email, age: @edad)
+
+	if @newuser.save
+		@respuesta = "Se guardaron los datos"
+	else
+		@respuesta = "Ocurrio un error"
+	end
+
+  end
 end
